9b4b6294a391b9f697abef625131d2d4aea96d27
Chris Frohmaier <cf5g09@soton.ac.uk>
2024-10-11 13:38:49 +0100

    Some tidying

--- uploadObjects/uploadApp.py
+++ uploadObjects/uploadApp.py
@@ -826,26 +826,12 @@ col_format = {
     'COMPLETENESS':np.float32,
     'PARALLAX':np.float32,
 }
-# cFormat = pd.Series(col_format)
-# print(cFormat)
-## We need to check if the uploaded CSV file matches the 
 
 uploaded_file = st.file_uploader("Choose a file")
 
 convertedDtype = 0
 meetMinFlag = 1
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    #st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    #st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
@@ -894,7 +880,7 @@ st.button(
     disabled= readyBitFlip
 )
     
-    #pd.testing.assert_series_equal(res,s)
+
 
 
 
